ae2de180d3157a5dfd651b8f8bf5f4573278a9af
gutodidonato <gutodidonato@gmail.com>
2025-07-17 16:21:06 -0300

    feat: ajuste deploy

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 @app.route("/tech")
 def get_tech_news():
     url = "https://www.theverge.com/tech"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    response = requests.get(url, headers=headers)
 
+    soup = BeautifulSoup(response.text, "html.parser")
     articles = []
 
-    for item in soup.select("a"):  
+    for item in soup.select("a"): 
         title = item.text.strip()
         articles.append(title)
-        
+
     return jsonify(articles)
 
 if __name__ == "__main__":
